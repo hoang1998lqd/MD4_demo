51d1b6a1bd3334f771d404a1f4d42a4a100a6020
theannguyen <104912107+theannguyen@users.noreply.github.com>
2022-09-03 21:52:07 +0700

    sửa lại query, đang làm câu 20

--- src/main/java/com/example/test_case/repository/IOrdersRepository.java
+++ src/main/java/com/example/test_case/repository/IOrdersRepository.java
@@ -3,13 +3,19 @@ package com.example.test_case.repository;
 import com.example.test_case.model.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface IOrdersRepository extends JpaRepository<Long, Orders> {
     //Trello #18
     @Query(value = "select * from orders where id like: id", nativeQuery = true)
-    List<Orders> findOrdersById (Long id);
+    List<Orders> findAllOrdersById (Long id);
+
+    @Query(value = "select * from orders where date_ship =: dsh and date_order =: dor", nativeQuery = true)
+    List<Orders> findAllOrdersByDateContaining(@Param("dsh")LocalDateTime dsh, @Param("dor")LocalDateTime dor);
+
 }
